b71a8f6fc487f508158a341fda5a4ecbfb2cd493
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-27 06:33:20 +0900

    python manage.py createsuperuser成功

--- api/urls.py
+++ api/urls.py
@@ -9,11 +9,11 @@ router = routers.DefaultRouter()
 router.register('tasks', TaskViewSet, basename='tasks')
 
 urlpatterns = [
-    path('list-post', PostListView(), name='list-post'),
+    path('list-post', PostListView().as_view(), name='list-post'),
     path('detail-post/<str:pk>/', PostRetrieveView.as_view(), name='detail-post'),
-    path('list-task', TaskListView(), name='list-task'),
+    path('list-task', TaskListView().as_view(), name='list-task'),
     path('detail-task/<str:pk>/', TaskRetrieveView.as_view(), name='detail-task'),
-    path('register/', CreateUserView(), name='register'),
+    path('register/', CreateUserView.as_view(), name='register'),
     path('auth/', include('djoser.urls.jwt')),
     path('', include(router.urls)),
 ]
