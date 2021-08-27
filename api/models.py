273389def6b3582ecbb3a8f4266531b1fc394544
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-27 09:27:59 +0900

    modelのカラム修正
    modelの内容を変更した時は
    ターミナルコマンド
    python manage.py migrate
    python manage.py makemigrations
    をする！

--- api/models.py
+++ api/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 
+
 class Post(models.Model):
 
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=500)
-    created_At = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.id) + " - " + self.title
 
+
 class Task(models.Model):
 
     title = models.CharField(max_length=50)
-    created_At = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.id) + " - " + self.title
